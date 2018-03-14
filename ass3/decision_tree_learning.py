dce7ebdd852e40ae79057bffa30fb198f67e7a21
kattn <mgbjerke@gmail.com>
2018-03-14 11:21:07 +0100

    Added helper classes

--- ass3/decision_tree_learning.py
+++ ass3/decision_tree_learning.py
@@ -7,8 +7,36 @@ attributes = (attribute_indecies)                 [0,1,2,3,4,5,6]
 parent_examples = [ ((attributes), classification) ]
 importance, the choosen importance function
 '''
-def decision_tree_learning(examples, attributes, parent_examples, importance):
 
+class Attr:
+    def __init__(self, id, val=-1):
+        self.id = id
+        self.val = val
+        self.pos_values = [1,2]
+
+
+class Case:
+    def __init__(self, attributes, target):
+        self.attributes = []
+        for i in range(len(attributes)):
+            self.attributes.append(Attr(i, attributes[i]))
+        self.target = target
+
+
+class Node:
+    def __init__(self, attr):
+        self.attr = attr
+        self.child = {}
+        self.classification = 0
+
+    def addChild(self, vk, node):
+        if(vk in self.attr.pos_values):
+            self.child[vk] = node
+        else:
+            raise Exception("Trying to add an invalid node")
+
+
+def decision_tree_learning(examples, attributes, parent_examples, importance):
     # If there are no examples return the plurality value
     if len( examples ) == 0:
         # print("No examples")
@@ -16,27 +44,30 @@ def decision_tree_learning(examples, attributes, parent_examples, importance):
 
     # If all examples have the same classification, return that
     for example in examples:
-        if example[1] != examples[0][1]:
+        if example.target != examples[0].target:
             break;
     else: # Will only run if the above for loop didn't break
         # print("All examples of same class")
-        return examples[0][1]
+        return examples[0].target
 
     # If there are no attributes
     if len( attributes ) == 0:
         # print("No attributes")
         return plurality_value(examples)
 
-    attr = max([(importance(attributes[a], examples),a) for a in range(len(attributes))]
+    attr = max([(importance(attr, examples), attr) for attr in attributes]
                , key=(lambda x: x[0]))[1]
+
     # tree[attr] has index of 0 and 1
-    tree = {attr: [None, None]}
-    for vk in range(attributes[attr]):
-        exs = [e for e in examples if e[0][attr] == vk]
+    tree = Node(attr)
+    for vk in attr.pos_values:  #values 1 and 2
+        exs = [e for e in examples if e.attributes[attr.id] == vk]
+        # continue from here
+
         exclude_attr = list(attributes)
-        del exclude_attr[attr]
+        del exclude_attr[attr.id]
         sub_tree = decision_tree_learning(exs, exclude_attr, examples, importance)
-        tree[attr][vk] = sub_tree
+        tree.addChild(vk, sub_tree)
 
     return tree
 
@@ -46,7 +77,7 @@ def plurality_value(examples):
     sum_zeros = 0
     sum_ones = 0
     for ex in examples:
-        if ex[1] == 1:
+        if ex.target == 1:
             sum_ones += 1
         else:
             sum_zeros += 1
@@ -54,21 +85,47 @@ def plurality_value(examples):
 
 
 def get_data(path):
-    examples = []
+    cases = []
     with open(path) as f:
         for line in f:
             line = line.strip().split("\t")
-            line = [ int(element)-1 for element in line ]
-            example = tuple(line[:-1]), line[-1]
-            examples.append( (example[0], example[1]) )
-    return examples
+            line = [ int(element) for element in line ]
+            cases.append( Case(line[:-1], line[-1]) )
+    return cases
+
+
+def print_tree(node):
+    open_set = [node]
+    while True:
+        current = open_set.pop(0)
+        if type(current) != type(0):
+            print("|" + str(current.attr.id), end="|\n")
+            for vk, node in current.child.items():
+                print("|" + str(vk), end="|")
+                open_set.append(node)
+
+        if len(open_set) == 0:
+            break
+
+
+def test_tree(self, node, examples):
+    correct_classification = 0
+    for example in examples:
+        current = node
+        # not done
 
 
 if __name__ == "__main__":
     init_examples = get_data("data/training.txt")
+    test_data = get_data("data/test.txt")
+
+    attrs = []
+    for i in range(len(init_examples[0].attributes)):
+        attrs.append(Attr(i))
 
     tree = decision_tree_learning( init_examples,
-                            [2]*len( init_examples[0][0] ),
-                            [],
-                            Importance.importance_ran)
-    print(tree)
+                                   attrs,
+                                   [],
+                                   Importance.importance_ran)
+
+    print_tree(tree)
